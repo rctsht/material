571e47aea03db967b79b69d15dc81cd1522c5da6
Michael Mason <mjmasn@gmail.com>
2018-10-11 17:55:31 +0100

    Re-export chip, icon, scrim exports from index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // @flow
 import './src/animation';
 
+export * from './src/chip';
+export * from './src/icon';
 export * from './src/screen';
+export * from './src/scrim';
 export * from './src/theme';
 export * from './src/touchable';
 export * from './src/type';
